81ecd0643668c701b4b4d96699e0421df2668905
Michael Troger <michaeltroger@users.noreply.github.com>
2016-12-03 23:22:24 +0100

    adjusted to android studio 2.2.2

--- app/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/m/Projects/Hello-libs1/app/src/main/cpp
+# Install script for directory: /home/m/Projects/feature-matching-native/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/m/Projects/Hello-libs1/app/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/m/Projects/feature-matching-native/app/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
