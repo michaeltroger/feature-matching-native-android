80921e8202b58c82072a8ce7ea6e356008edb010
Michael Troger <dev@michaeltroger.com>
2021-08-20 19:48:05 +0200

    Update gradle

---
+++ app/.cxx/cmake/debug/x86/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.4.0-70-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-70-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/m/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-14")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "14")
+set(CMAKE_SYSTEM_PROCESSOR "i686")
+
+set(CMAKE_ANDROID_NDK "/home/m/Android/Sdk/ndk-bundle")
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN "")
+set(CMAKE_ANDROID_ARCH "x86")
+set(CMAKE_ANDROID_ARCH_ABI "x86")
+set(CMAKE_ANDROID_ARCH_HEADER_TRIPLE "i686-linux-android")
+set(CMAKE_ANDROID_NDK_DEPRECATED_HEADERS "0")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
