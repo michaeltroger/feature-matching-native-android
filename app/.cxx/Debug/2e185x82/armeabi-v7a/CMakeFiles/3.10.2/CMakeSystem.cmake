23dfb2959bef7c3ef2fa4b93e4f9c5d61858914c
Michael Troger <dev@michaeltroger.com>
2021-08-20 20:31:26 +0200

    Fix platform param

---
+++ app/.cxx/Debug/2e185x82/armeabi-v7a/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,24 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.4.0-70-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-70-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/m/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-14")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "14")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_ANDROID_NDK "/home/m/Android/Sdk/ndk-bundle")
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN "")
+set(CMAKE_ANDROID_ARCH "arm")
+set(CMAKE_ANDROID_ARCH_ABI "armeabi-v7a")
+set(CMAKE_ANDROID_ARCH_HEADER_TRIPLE "arm-linux-androideabi")
+set(CMAKE_ANDROID_NDK_DEPRECATED_HEADERS "0")
+set(CMAKE_ANDROID_ARM_MODE "1")
+set(CMAKE_ANDROID_ARM_NEON "0")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
